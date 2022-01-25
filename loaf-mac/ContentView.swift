36afc874b1e3e2a030d84afa722181bf76e78779
yejunjie10311031 <62139629+yejunjie10311031@users.noreply.github.com>
2022-01-25 22:26:48 +0800

    Add files via upload

---
+++ loaf-mac/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  loaf-mac
+//
+//  Created by yejunjie on 2022/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("wuguo")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ loaf-mac/loaf_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  loaf_macApp.swift
+//  loaf-mac
+//
+//  Created by yejunjie on 2022/1/25.
+//
+
+import SwiftUI
+
+@main
+struct loaf_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
